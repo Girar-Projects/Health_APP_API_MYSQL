ed77ea720df199c2a4e855ee60f6c1aa63f28e8e
Bereket Zelalem <64479153+bereket-09@users.noreply.github.com>
2023-08-10 17:28:58 +0300

    Updated registor

--- app.js
+++ app.js
@@ -37,62 +37,140 @@ app.post("/login", (req, res) => {
   );
 });
 
-app.post("/register", (req, res) => {
-  const data = req.body;
-
-  console.log(data);
+app.post('/register', (req, res) => {
+  const { uuid, email, password, user_type, paymentStatus, profileCreationStatus, longitude, latitude } = req.body;
+  
   // Perform validation on the request data
   if (
-    typeof data.uuid !== "string" ||
-    typeof data.email !== "string" ||
-    typeof data.password !== "string" ||
-    typeof data.user_type !== "string" ||
-    typeof data.paymentStatus !== "string" ||
-    typeof data.profileCreationStatus !== "string" ||
-    isNaN(parseFloat(data.longitude)) ||
-    isNaN(parseFloat(data.latitude)) ||
-    !data.uuid ||
-    !data.email ||
-    !data.password ||
-    !data.user_type ||
-    !data.paymentStatus ||
-    !data.profileCreationStatus
+    typeof uuid !== "string" ||
+    typeof email !== "string" ||
+    typeof password !== "string" ||
+    typeof user_type !== "string" ||
+    typeof paymentStatus !== "string" ||
+    typeof profileCreationStatus !== "string" ||
+    isNaN(parseFloat(longitude)) ||
+    isNaN(parseFloat(latitude)) ||
+    !uuid ||
+    !email ||
+    !password ||
+    !user_type ||
+    !paymentStatus ||
+    !profileCreationStatus
   ) {
-    res.status(400).json({
+    return res.status(400).json({
+      statusCode: 400,
       message:
         "uuid, email, password, user_type, paymentStatus, profileCreationStatus, longitude and latitude are required and should be of the correct data type",
     });
-    return;
   }
 
-  if (!data.email.includes("@")) {
-    res.status(400).json({ message: "Invalid email address" });
-    return;
+  if (!email.includes("@")) {
+    return res.status(400).json({ 
+      statusCode: 400,
+      message: "Invalid email address" 
+    });
   }
 
   // Insert the new user into the users table
   const sql =
     "INSERT INTO users (uuid, email, password, user_type, paymentStatus, profileCreationStatus, longitude, latitude) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
   const values = [
-    data.uuid,
-    data.email,
-    data.password,
-    data.user_type,
-    data.paymentStatus,
-    data.profileCreationStatus,
-    parseFloat(data.longitude),
-    parseFloat(data.latitude),
+    uuid,
+    email,
+    password,
+    user_type,
+    paymentStatus,
+    profileCreationStatus,
+    parseFloat(longitude),
+    parseFloat(latitude),
   ];
 
   connection.query(sql, values, (err, result) => {
     if (err) {
       console.error("Error registering new user: " + err.stack);
-      res.sendStatus(500);
+      return res.status(500).json({
+        statusCode: 500,
+        message: 'Internal server error'
+      });
     } else {
-      res.json({ message: "User registered successfully" });
+      const userId = result.insertId; // Get the ID of the newly inserted user
+
+      // Generate a JWT token for the new user
+      const token = jwt.sign(
+        { id: userId, type: user_type },
+        secretKey,
+        { expiresIn: '1h' }
+      );
+            
+      return res.status(201).json({ 
+        statusCode: 201,
+        message: "User registered successfully",
+        user_id: userId,
+        token: token,
+        userType: user_type
+      });
     }
   });
 });
+
+// app.post("/register", (req, res) => {
+//   const data = req.body;
+
+//   console.log(data);
+//   // Perform validation on the request data
+//   if (
+//     typeof data.uuid !== "string" ||
+//     typeof data.email !== "string" ||
+//     typeof data.password !== "string" ||
+//     typeof data.user_type !== "string" ||
+//     typeof data.paymentStatus !== "string" ||
+//     typeof data.profileCreationStatus !== "string" ||
+//     isNaN(parseFloat(data.longitude)) ||
+//     isNaN(parseFloat(data.latitude)) ||
+//     !data.uuid ||
+//     !data.email ||
+//     !data.password ||
+//     !data.user_type ||
+//     !data.paymentStatus ||
+//     !data.profileCreationStatus
+//   ) {
+//     res.status(400).json({
+//       message:
+//         "uuid, email, password, user_type, paymentStatus, profileCreationStatus, longitude and latitude are required and should be of the correct data type",
+//     });
+//     return;
+//   }
+
+//   if (!data.email.includes("@")) {
+//     res.status(400).json({ message: "Invalid email address" });
+//     return;
+//   }
+
+//   // Insert the new user into the users table
+//   const sql =
+//     "INSERT INTO users (uuid, email, password, user_type, paymentStatus, profileCreationStatus, longitude, latitude) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+//   const values = [
+//     data.uuid,
+//     data.email,
+//     data.password,
+//     data.user_type,
+//     data.paymentStatus,
+//     data.profileCreationStatus,
+//     parseFloat(data.longitude),
+//     parseFloat(data.latitude),
+//   ];
+
+//   connection.query(sql, values, (err, result) => {
+//     if (err) {
+//       console.error("Error registering new user: " + err.stack);
+//       res.sendStatus(500);
+//     } else {
+//       res.json({ message: "User registered successfully" });
+//     }
+//   });
+// });
+
+
 
 // Endpoint for updating user password
 app.put("/update-password/:user_id", (req, res) => {
