985eef7d1b1e33d3d6a37a75b529b77d39a85a3b
Bereket Zelalem <64479153+bereket-09@users.noreply.github.com>
2023-10-14 10:20:39 +0300

    DB updated

--- db.js
+++ db.js
@@ -2,20 +2,47 @@ const mysql = require("mysql2");
 
 const dbAddresses = ["mysql", "db4free.net", "localhost"];
 
+// const connection = mysql.createConnection({
+//   host: "mysql",
+//   port: 3307,
+//   user: "health_testing",
+//   password: "0919483800",
+//   database: "health_app_test",
+//   multipleStatements: true,
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting t o database: " + err.message);
+//     return;
+//   }
+//   console.log("Connected to MySQL database");
+// });
+
+// module.exports = connection;
+
+
+
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "196.188.127.211",
+  port: 3307,
   user: "health_testing",
   password: "0919483800",
   database: "health_app_test",
   multipleStatements: true,
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to database: " + err.message);
-    return;
-  }
-  console.log("Connected to MySQL database");
-});
+function connect() {
+  connection.connect((err) => {
+    if (err) {
+      console.error("Error connecting to database: " + err.message);
+      setTimeout(connect, 5000); // retry every 5 seconds
+      return;
+    }
+    console.log("Connected to MySQL database");
+  });
+}
+
+connect();
 
 module.exports = connection;
